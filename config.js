8e7342afe716eae6aa61467c78210b49edccf9bd
anu bazarragchaa <38427020+anu-bazar@users.noreply.github.com>
2023-06-24 22:11:17 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+// Database Configuration
+
+const config = {
+  database: {
+    host: 'localhost',
+    port: 5432,
+    name: 'your_database_name',
+    username: 'your_database_username',
+    password: 'your_database_password'
+  },
+  server: {
+    port: 3000
+  },
+  // Top secret super secret configurations :)) 
+};
+
+module.exports = config;
