397b2bf2382b98111ac4db24e9321be85ca7d435
anu bazarragchaa <38427020+anu-bazar@users.noreply.github.com>
2023-06-24 22:12:42 +0200

    Create taskSchema.js

---
+++ taskSchema.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('./database'); 
+
+const Task = sequelize.define('Task', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  dueDate: {
+    type: DataTypes.DATE,
+    allowNull: true
+  }
+});
+
+module.exports = Task;
